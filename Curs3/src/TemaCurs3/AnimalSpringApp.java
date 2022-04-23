ac548b7864279576e70278fc08da612a8715adbd
nichi <nihciteleamarius@gmail.com>
2022-04-23 22:42:26 +0300

    Tema curs 15

--- Curs3/src/TemaCurs3/AnimalSpringApp.java
+++ Curs3/src/TemaCurs3/AnimalSpringApp.java
@@ -1,20 +1,19 @@
 package TemaCurs3;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class AnimalSpringApp {
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter your animal name:");
-//        String name = sc.nextLine();
-        IAnimal theCat = context.getBean("myCat", IAnimal.class);
-        IAnimal theDog = context.getBean("myDog", IAnimal.class);
-
-        System.out.println(theCat.makeSound());
-        System.out.println(theDog.getAnimalName());
-        System.out.println(theDog.makeSound());
-
-        context.close();
-    }
-}
+//
+//public class AnimalSpringApp {
+//    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+////        Scanner sc = new Scanner(System.in);
+////        System.out.println("Enter your animal name:");
+////        String name = sc.nextLine();
+//        IAnimal theCat = context.getBean("myCat", IAnimal.class);
+//        IAnimal theDog = context.getBean("myDog", IAnimal.class);
+//
+//        System.out.println(theCat.makeSound());
+//        System.out.println(theDog.getAnimalName());
+//        System.out.println(theDog.makeSound());
+//
+//        context.close();
+//    }
+//}
